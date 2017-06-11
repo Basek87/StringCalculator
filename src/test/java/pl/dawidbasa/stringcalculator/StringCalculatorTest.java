eb96191717d451fda1f19578dbca1aaf672bd831
basek87 <dawidbasa@gmail.com>
2017-06-11 13:19:57 +0200

    Tests for empty, separated by comma and new lines Strings are passed.

--- src/test/java/pl/dawidbasa/stringcalculator/StringCalculatorTest.java
+++ src/test/java/pl/dawidbasa/stringcalculator/StringCalculatorTest.java
@@ -1,5 +1,45 @@
 package pl.dawidbasa.stringcalculator;
 
-public class StringCalculatorTest {
+import static org.junit.Assert.assertEquals;
 
+import org.junit.Test;
+
+public class StringCalculatorTest {
+	
+	@Test
+	public void emptyStringShouldReturnZero(){
+		StringCalculator calculator = new StringCalculator();
+		assertEquals(calculator.calculate(""), 0);
+	}
+	
+	@Test
+	public void singleStringShouldReturnSameValue(){
+		StringCalculator calculator = new StringCalculator();
+		assertEquals(calculator.calculate("1"), 1);
+		assertEquals(calculator.calculate("11"), 11);
+	}
+	
+	@Test
+	public void commaSeparatedStringsShouldReturnSum(){
+		StringCalculator calculator = new StringCalculator();
+		assertEquals(calculator.calculate("1,2"), 3);
+		assertEquals(calculator.calculate("21,12"), 33);
+	}
+	
+	@Test
+	public void multiCommasSeparatedStringsShouldReturnSum(){
+		StringCalculator calculator = new StringCalculator();
+		assertEquals(calculator.calculate("1,1,1"), 3);
+		assertEquals(calculator.calculate("1,2,3,4"), 10);
+	}
+	
+	@Test
+	public void multiCommaAndNewLinesSeparatedStringsShouldReturnSum(){
+		StringCalculator calculator = new StringCalculator();
+		assertEquals(calculator.calculate("1\n1"), 2);
+		assertEquals(calculator.calculate("1\n2,3"), 6);
+		
+		
+	}
 }
+ 
