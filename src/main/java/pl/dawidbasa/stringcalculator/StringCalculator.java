e8727bca006ba37e11f6bc24e9f42567a52bdc56
basek87 <dawidbasa@gmail.com>
2017-06-26 10:47:30 +0200

    new Methods checkForNegatives and getNumbers(ignore numbers bigger than 1000)

--- src/main/java/pl/dawidbasa/stringcalculator/StringCalculator.java
+++ src/main/java/pl/dawidbasa/stringcalculator/StringCalculator.java
@@ -1,31 +1,68 @@
+/* tests passed */
+// 1. Method adds strings (0,1,2, or more) if empty return 0
+// 2. Allow the Add method to handle new lines between numbers (instead of
+// commas).
+// 3. Support different delimiters - example “//;\n1;2” should return three
+// where the default delimiter
+// 4.Calling Add with a negative number will throw an exception “negatives not allowed” - and the negative that was passed.if there are multiple negatives, show all of them in the exception message	
+// 5.Numbers bigger than 1000 should be ignored, so adding 2 + 1001  = 2
+
 package pl.dawidbasa.stringcalculator;
 
-import java.util.Arrays;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
 public class StringCalculator {
 
-	/* tests passed */
-	// 1. Method adds strings (0,1,2, or more) if empty return 0
-	// 2. Allow the Add method to handle new lines between numbers (instead of
-	// commas).
+	public String delimiter;
+	public String numbers;
+
+	public StringCalculator() {
+	}
+
+	public StringCalculator(String delimiter, String numbers) {
+		super();
+		this.delimiter = delimiter;
+		this.numbers = numbers;
+	}
 
 	public int calculate(String input) {
-
-		if (input.isEmpty()) {
-			return 0;
-		}
-		Stream<String> list = Arrays.stream(input.split(",|\n"));
-		return list.mapToInt(Integer::parseInt).sum();
-
-		// 3. Support different delimiters - todo in next step
-		/*
-		 * to change a delimiter, the beginning of the string will contain a
-		 * separate line that looks like this: “//[delimiter]\n[numbers…]” for
-		 * example “//;\n1;2” should return three where the default delimiter is
-		 * ‘;’ . the first line is optional. all existing scenarios should still
-		 * be supported
-		 */
-
+		return checkInput(input).sum();
 	}
+
+	private int sum() {
+		checkForNegativeNumbers();
+		return getNumbers().sum();
+	}
+
+	private void checkForNegativeNumbers() {
+		String negativeNumbers = getNumbers()
+				.filter(n -> n < 0)
+				.mapToObj(Integer::toString)
+				.collect(Collectors.joining(","));
+		if (!negativeNumbers.isEmpty()) {
+			throw new IllegalArgumentException("negatives not allowed " + negativeNumbers);
+		}
+	}
+
+	private IntStream getNumbers() {
+		if (numbers.isEmpty()) {
+			return IntStream.empty();
+		}
+		return Stream.of(numbers
+				.split(delimiter))
+				.mapToInt(Integer::parseInt)
+				.map(n -> n % 1000);
+	}
+
+	private StringCalculator checkInput(String input) {
+		if (input.startsWith("//")) {
+			String[] parts = input.split("\n", 2);
+			return new StringCalculator(parts[0].substring(2), parts[1]);
+		} else {
+			return new StringCalculator(",|\n", input);
+		}
+	}
+
 }
